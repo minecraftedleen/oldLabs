5e2479084fabf85cdc3d1c60ced7570812ca62af
BT_1N3_23 <jackeyh2@Nycstudents.net>
2023-10-31 10:44:00 -0400

    fourth commit

--- src/ExactChange.java
+++ src/ExactChange.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class ExactChange {
     public static void main(String[] args) {
         final double QCONST = 0.25;
@@ -8,5 +9,26 @@ public class ExactChange {
         int dimes;
         int nickels;
         int pennies;
+        int minCoins;
+        double cash;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter an amount in dollars and cents: ");
+        cash = scan.nextDouble();
+        quarters = (int) (cash / QCONST);
+        cash %= QCONST;
+        dimes = (int) (cash / DCONST);
+        cash %= DCONST;
+        nickels = (int) (cash / NCONST);
+        cash %= NCONST;
+        pennies = (int) (cash / PCONST + 0.5);
+        cash %= PCONST;
+        minCoins = quarters + dimes + nickels + pennies;
+        System.out.println("The minimum number of coins is: " + minCoins);
+        System.out.println(quarters + " quarters");
+        System.out.println(dimes + " dimes");
+        System.out.println(nickels + " nickels");
+        System.out.println(pennies + " pennies");
     }
 }
