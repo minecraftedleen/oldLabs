9fdc019e5c75948867a03a55f98f7696fa4a1af0
BT_1N3_23 <jackeyh2@Nycstudents.net>
2023-10-31 10:27:54 -0400

    third commit

---
+++ src/ExactChange.java
@@ -0,0 +1,12 @@
+public class ExactChange {
+    public static void main(String[] args) {
+        final double QCONST = 0.25;
+        final double DCONST = 0.10;
+        final double NCONST = 0.05;
+        final double PCONST = 0.01;
+        int quarters;
+        int dimes;
+        int nickels;
+        int pennies;
+    }
+}
